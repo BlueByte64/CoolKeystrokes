e54aa9db1f0694593626531b3bbcb0682e7d23ec
QByte64 <bugadojogando@gmail.com>
2025-02-01 13:27:28 -0300

    Just started the json Serializer and Deserializer, changed some stuff on project settings

--- InfoHandling/JsonReader.cs
+++ InfoHandling/JsonReader.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace KeystrokesByQByte64.InfoHandling
 {
-    internal class JsonReader
+    internal class JsonHandler
     {
+        class JsonStructure{
+            
+        }
     }
 }
 
